cd9732b21bae370e9f84057094eccda5bf5e8993
Emil Kjelsrud <emilkje@gmail.com>
2012-06-15 01:37:27 +0200

    Added some UI enhancements.

--- weather_api.php
+++ weather_api.php
@@ -19,7 +19,7 @@ $days = array(
 $i = 0;
 foreach($dom->forecast->tabular->time as $item) {
 	$dom->forecast->tabular->time[$i]->day = $days[Date("D", strtotime($item->attributes()->from))];
-	$dom->forecast->tabular->time[$i]->date = Date("d/m/y", strtotime($item->attributes()->from));
+	$dom->forecast->tabular->time[$i]->date = Date("d.m. Y", strtotime($item->attributes()->from));
 	$dom->forecast->tabular->time[$i]->time = Date("H:i", strtotime($item->attributes()->from));
 	$dom->forecast->tabular->time[$i]->time .= " - " . Date("H:i", strtotime($item->attributes()->to));
 	$i++;
